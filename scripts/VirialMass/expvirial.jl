3893abe308f1e0811ccd4eb5d33844e889a8d70a
ngiann <ngiann@gmail.com>
2022-06-07 21:41:21 +0200

    pass more arguments to properrun

--- scripts/VirialMass/expvirial.jl
+++ scripts/VirialMass/expvirial.jl
@@ -3,13 +3,13 @@ using JLD2
 using ProgressMeter, Suppressor, Printf
 using GPCCData
 
-function formdelays(source)
+function formdelays(source, Δt)
 
     tobs, = readdataset(source = source);
 
     τmax = maximum(maximum.(tobs)) - minimum(minimum.(tobs))
 
-    τ = 0.0:0.05:τmax
+    τ = 0.0:Δt:τmax
 
     L = length(tobs)
 
@@ -23,7 +23,7 @@ function runme(source; maxiter=1, numberofrestarts=1, rhomax = rhomax, kernel = 
 
     tobs, yobs, σobs, = readdataset(source = source);
 
-    @printf("Trying out %d delay combinations in parallel\n", length(delays))
+    @printf("Trying out %d delay combinations in parallel with kernel %s\n", length(delays), string(kernel))
 
     @showprogress pmap(d->(@suppress performcv(tobs, yobs, σobs; rhomax=rhomax, iterations = maxiter, numberofrestarts = numberofrestarts, delays = [0;collect(d)], kernel = kernel)), delays)
 
@@ -35,15 +35,15 @@ runme("3C120"; maxiter=1, numberofrestarts=1, rhomax = 10, kernel = GPCC.OU, del
 runme("3C120"; maxiter=1, numberofrestarts=1, rhomax = 10, kernel = GPCC.OU, delays = LinRange(0.0, 10, 2*nworkers()))
 
 
-function properrun(kernel, rhomax = 1000.0)
+function properrun(; kernel, rhomax = 1000.0, Δt = 0.1, numberofrestarts = 7, name = "")
 
     for source in listvirialdatasets()
 
-        delays = formdelays(source)
+        delays = formdelays(source, Δt)
 
-        RESULTS = runme(source, maxiter = 3000, numberofrestarts = 13, rhomax = rhomax, kernel = kernel, delays = delays)
+        RESULTS = runme(source, maxiter = 3000, numberofrestarts = numberofrestarts, rhomax = rhomax, kernel = kernel, delays = delays)
 
-        JLD2.save("results_"*source*@sprintf("_%.2f_", rhomax)*string(kernel)*".jld2", "cvresults", RESULTS, "delays", collect(delays))
+        JLD2.save("results_" * name * source * @sprintf("_rho_%f_K_", rhomax) * string(kernel) * "_Dt_" * string(Δt) * ".jld2", "cvresults", RESULTS, "delays", collect(delays))
 
     end
 
