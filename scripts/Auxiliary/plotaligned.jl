006940608ecb3c0c6ed82e26556ea7a3c58d1d27
ngiann <ngiann@gmail.com>
2022-07-20 12:39:45 +0200

    gpcc returns posterior of shift vector

--- scripts/Auxiliary/plotaligned.jl
+++ scripts/Auxiliary/plotaligned.jl
@@ -1,12 +1,12 @@
 using Statistics, StatsFuns, PyPlot
 
-function plotaligned(tobs, yobs; kernel = kernel, delays = delays, rhomax = rhomax, numberofrestarts = 7, iterations = 1000)
+function plotaligned(tobs, yobs, σobs; kernel = kernel, delays = delays, rhomax = rhomax, numberofrestarts = 7, iterations = 1000)
 
-    minopt, pred, (α, b, ρ) = gpcc(tobs, yobs, σobs;
+    minopt, pred, (α, posterioroffsetb, ρ) = gpcc(tobs, yobs, σobs;
         kernel = kernel, delays = delays, iterations = iterations, numberofrestarts = numberofrestarts,
         initialrandom=1, rhomin = 0.1, rhomax = rhomax)
 
-    # b = mean(posterioroffsetb)
+    b = mean(posterioroffsetb)
 
     @assert(length(tobs) == length(yobs) == length(delays) == length(b) == length(α) == 2)
 
@@ -24,9 +24,9 @@ function plotaligned(tobs, yobs; kernel = kernel, delays = delays, rhomax = rhom
     xtest = minimum(minimum.(tobs)):(interval/1000):maximum(maximum.(tobs))
 
 
-    PyPlot.plot(xtest .- delays[1], (1 / α[1]) * (pred(xtest)[1][1] .- b[1]))
+   # PyPlot.plot(xtest .- delays[1], (1 / α[1]) * (pred(xtest)[1][1] .- b[1]))
 
-    PyPlot.plot(xtest .- delays[2], (1 / α[2]) * (pred(xtest)[1][2] .- b[2]), "--")
+   # PyPlot.plot(xtest .- delays[2], (1 / α[2]) * (pred(xtest)[1][2] .- b[2]), "--")
 
 
 end
