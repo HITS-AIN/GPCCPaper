5b5faeda2f30acbae3f5985d397a50ce4e894674
ngiann <ngiann@gmail.com>
2022-11-03 15:28:03 +0100

    remove old images

--- scripts/Synthetic/exp1.jl
+++ scripts/Synthetic/exp1.jl
@@ -13,9 +13,9 @@ function runme(σ; maxiter=1, numberofrestarts=1)
 
     @printf("Trying out %d delay combinations in parallel\n", length(delays))
 
-    out = @showprogress pmap(τ -> (@suppress gpcc(tobs, yobs, σobs; iterations = maxiter, rhomax = 300, numberofrestarts = numberofrestarts, delays = [0;τ], kernel = GPCC.matern32)[1]), delays)
+    loglikel = @showprogress pmap(τ -> (@suppress gpcc(tobs, yobs, σobs; iterations = maxiter, rhomax = 300, numberofrestarts = numberofrestarts, delays = [0;τ], kernel = GPCC.matern32)[1]), delays)
 
-    return out, delays,tobs, yobs, σobs
+    return loglikel, delays,tobs, yobs, σobs
 
 end
 
@@ -29,13 +29,13 @@ function properrun()
 
     for σ in [0.01; 0.1; 0.2; 0.5; 1.0; 1.5]
 
-        out, delays, tobs, yobs, σobs  = runme(σ; maxiter = 1000, numberofrestarts = 5)
+        loglikel, delays, tobs, yobs, σobs  = runme(σ; maxiter = 1000, numberofrestarts = 5)
 
         filename = @sprintf("results_synthetic_%.2f.jld2", σ)
 
         @printf("Writing results in file %s\n", filename)
 
-        JLD2.save(filename, "out", out, "delays", collect(delays), "tobs", tobs, "yobs", yobs, "σobs", σobs)
+        JLD2.save(filename, "loglikel", loglikel, "delays", collect(delays), "tobs", tobs, "yobs", yobs, "σobs", σobs)
 
     end
 
