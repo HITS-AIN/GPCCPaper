b703d40c42f6f2f1287b83b364502dcc784b9b2c
ngiann <ngiann@gmail.com>
2022-11-03 15:20:23 +0100

    script for GPCCData experiments included in paper

---
+++ scripts/GPCCDataExperiments/runme.jl
@@ -0,0 +1,75 @@
+# Start julia with "julia -O3 -p16" where 16 means 16 parallel workers.
+# Alternatively, use the two lines below to start within julia multiple parallel workers.
+# using Distributed
+# addprocs(16)
+
+@everywhere using GPCC
+@everywhere using ProgressMeter, Suppressor # need to be indepedently installed
+
+using GPCCData  # needs to be indepedently installed
+using PyPlot    # needs to be indepedently installed
+using Printf
+using JLD2      # needs to be indepedently installed
+
+
+function getlikelihoods(datasetname; WARMUP = WARMUP)
+
+    tobs, yobs, σobs,  = readdataset(source = datasetname)
+
+    candidatedelays = collect(0.0:0.2:140)
+
+
+    if WARMUP
+
+        @printf("\nWARMUP\n")
+
+        @showprogress pmap(candidatedelays[1:2*nworkers()]) do delay
+
+            @suppress gpcc(tobs, yobs, σobs; kernel = GPCC.OU, iterations=3,rhomin=0.01, rhomax = 20, delays = [0; delay], numberofrestarts=1)[1]
+            
+        end
+
+        return candidatedelays, zeros(length(candidatedelays))
+
+    end
+
+
+    loglikel = @showprogress pmap(candidatedelays) do delay
+
+        @suppress gpcc(tobs, yobs, σobs; kernel = GPCC.OU, iterations = 2000, rhomin=0.01, rhomax = 2000, delays = [0; delay], numberofrestarts = 7, seed = 1)[1]
+        
+    end
+
+    return candidatedelays, loglikel
+
+end
+
+
+function runalldatasets()
+
+    datasets = ["3C120", "Mrk335", "Mrk6", "Mrk1501", "PG2130099"]
+
+    for d in datasets
+
+        filename = @sprintf("loglikel_%s.jld2", d)
+
+        @printf("\nGetting log-likelihoods for datasets %s\n", d)
+
+        @printf("\tStore result in file called %s\n\n", filename)
+        
+        candidatedelays, loglikel = getlikelihoods("3C120"; WARMUP = false)
+
+        JLD2.save(filename, "candidatedelays", candidatedelays, "loglikel",  loglikel,  "d", d)
+
+    end
+
+end
+
+# Do warmup
+getlikelihoods("3C120"; WARMUP = true)
+getlikelihoods("3C120"; WARMUP = true)
+
+# Calculate log-likelihoods and store them in JLD2 files
+runalldatasets()
+
+
