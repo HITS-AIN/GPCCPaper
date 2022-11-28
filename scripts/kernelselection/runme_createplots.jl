0cd44f6cdaf9968f233f1355bbfbfc78f6fa7a46
ngiann <ngiann@gmail.com>
2022-11-28 20:56:14 +0100

    changes labels for y axes

--- scripts/kernelselection/runme_createplots.jl
+++ scripts/kernelselection/runme_createplots.jl
@@ -43,7 +43,7 @@ function createplots()
 
     fig = GLMakie.Figure(resolution = resolution, fontsize = fontsize)
    
-    ax1 = Axis(fig[1,1], xlabel = "Days", ylabel = "log-likelihood")
+    ax1 = Axis(fig[1,1], xlabel = "Days", ylabel = "CV log-likelihood")
 
 
     # plot data
@@ -100,7 +100,7 @@ function createplots()
 
     fig = GLMakie.Figure(resolution = resolution, fontsize = fontsize)
    
-    ax1 = Axis(fig[1,1], xlabel = "Days", ylabel = "log-likelihood")
+    ax1 = Axis(fig[1,1], xlabel = "Days", ylabel = "CV log-likelihood")
 
 
     # plot data
